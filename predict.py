fd8679dedbed29dc0157243f26de767f345e246f
Vincent QB <vincentqb@users.noreply.github.com>
2016-01-03 02:30:00 -0500

    Inline filenames. Apostrophes for strings.

--- predict.py
+++ predict.py
@@ -2,27 +2,22 @@ from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import pandas as pd
 
-# File names
-train_file_name = 'train.csv'
-test_file_name = 'test.csv'
-predict_file_name = 'predict.csv'
-
 # Read training data
-train_data = pd.read_csv(train_file_name)
+train_data = pd.read_csv('train.csv')
 target = train_data['label'].values
 train = train_data.iloc[:,1:].values
 
 # Read test data
-test = pd.read_csv(test_file_name).values
+test = pd.read_csv('test.csv').values
 
-# Use Classifier to create model and make prediction
+# Use classifier to create model and make prediction
 rf = RandomForestClassifier(n_estimators=100)
 rf.fit(train, target)
 predict = rf.predict(test)
 
-# Save result
+# Save results
 predict_table = np.c_[range(1,len(test)+1), predict]
-np.savetxt(predict_file_name, predict_table, header="ImageId,Label", comments="", delimiter=",", fmt="%d")
+np.savetxt('predict.csv', predict_table, header='ImageId,Label', comments='', delimiter=',', fmt='%d')
 
 # Visualize
 # import matplotlib.pyplot as plt
