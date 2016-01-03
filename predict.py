9e07b5a98279e0f44841472c9b045f9c8f2d7c15
Vincent QB <vincentqb@users.noreply.github.com>
2016-01-02 22:42:52 -0500

    Random Forest Classifier from sklearn.

---
+++ predict.py
@@ -0,0 +1,28 @@
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+import pandas as pd
+
+# File names
+train_file_name = 'train.csv'
+test_file_name = 'test.csv'
+predict_file_name = 'predict.csv'
+
+# Read training data
+train_data = pd.read_csv(train_file_name, header=0)
+# train_data = pd.read_csv(train_file_name)
+target = train_data['label'].values
+train = train_data.iloc[:,1:].values
+
+# Read test data
+test = pd.read_csv(test_file_name, header=0).values
+# test = pd.read_csv(test_file_name).values
+
+# Use Classifier to create model and make prediction
+rf = RandomForestClassifier()
+rf.fit(train, target)
+predict = rf.predict(test)
+
+# Save result
+# np.savetxt(predict_file_name, predict, delimiter=",", fmt="%d")
+predict_concatenated = np.c_[range(1,len(test)+1), predict]
+np.savetxt(predict_file_name, predict_concatenated, header="ImageId,Label", comments="", delimiter=",", fmt="%d")
