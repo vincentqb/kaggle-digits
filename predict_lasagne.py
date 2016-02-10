75b13f751252fe6cd3fb8d8d01653f9ea7de1c44
Vincent QB <vincentqb@users.noreply.github.com>
2016-02-10 10:59:11 -0500

    focus on lasagne.

--- predict_lasagne.py
+++ predict_lasagne.py
@@ -29,6 +29,8 @@ from lasagne.updates import nesterov_momentum
 from lasagne.nonlinearities import softmax, rectify
 from nolearn.lasagne import NeuralNet
 
+# Data needs to be shaped as a matrix
+# train = train.reshape(-1,28,28)
 clf = NeuralNet(
     layers = [
     ('input', layers.InputLayer),
@@ -64,30 +66,41 @@ clf = NeuralNet(
     verbose = 1,
     )
 
+# Data needs to be shaped as one long vector
+# train = train.reshape(-1,28*28)
+clf = NeuralNet(
+    layers = [  
+        # Three layers: one hidden layer
+        ('input', layers.InputLayer),
+        ('hidden1', layers.DenseLayer),
+        # ('hidden2', layers.DenseLayer),
+        ('output', layers.DenseLayer),
+        ],
+
+    # Layer parameters
+    input_shape = (None, 28*28),
+    hidden1_num_units = 100,         # Number of units in hidden layer (10, 1000, ...)
+    # hidden2_num_units = 100,       # Number of units in hidden layer (10, 1000, ...)
+    output_nonlinearity = softmax,   # Output layer uses identity function
+    output_num_units = 10,           # Output 10 target values for the digits 0, 1, 2, ..., 9
+
+    # Optimization method
+    update = nesterov_momentum,
+    update_learning_rate = 0.001,   # 0.01, 0.001, 0.0001, ...
+    update_momentum = 0.9,
+    max_epochs = 15,
+
+    verbose = 1,
+    )
+
+# clf = NeuralNet()
+
+### Convert to appropriate data type
+
 # Theano is strict on the format of floats and ints
 train = train.astype(np.float32)
 label = label.astype(np.int32)
 test = test.astype(np.float32)
-
-### Optimize classifer's parameters
-
-def grid_search(train, label, params):
-    from sklearn.grid_search import GridSearchCV
-    
-    # Search on fewer entries
-    label_few = label[0::10]
-    train_few = train[0::10]
-    
-    # Run exhaustive grid search
-    start = clock()
-    gs = GridSearchCV(estimator = clf, param_grid = params, n_jobs = 2)
-    gs.fit(train_few, label_few)
-    print("Parameter optimized {} yielding {:.4f} in {:.0f} seconds.".format(
-        gs.best_params_, gs.best_score_, clock() - start))
-    
-# Parameter space to search
-# params = [{'C': np.logspace(-1, 3), 'gamma': np.logspace(-4, -1)}]
-# grid_search(train, label, params)
 
 ### Cross validation
 
